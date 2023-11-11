fb867cf50b53a9eb6f0a469bf97993a29d1c91d3
Venomity <veeti.hietasalo@hotmail.fi>
2023-11-11 14:21:22 +0200

    tietokannan luontiscriptin päivitys

--- albumscript.sql
+++ albumscript.sql
@@ -14,13 +14,72 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for albums
+CREATE DATABASE IF NOT EXISTS `albums` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `albums`;
+
+-- Dumping structure for table albums.album
+CREATE TABLE IF NOT EXISTS `album` (
+  `id` bigint(20) NOT NULL,
+  `albumname` varchar(255) DEFAULT NULL,
+  `artist` varchar(255) DEFAULT NULL,
+  `coverimage` double NOT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `releaseyear` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table albums.albums
+CREATE TABLE IF NOT EXISTS `albums` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `albumname` varchar(100) DEFAULT NULL,
+  `artist` varchar(100) DEFAULT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `coverimage` varchar(15) DEFAULT NULL,
+  `releaseyear` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Data exporting was unselected.
+
+-- Dumping structure for table albums.album_seq
+CREATE TABLE IF NOT EXISTS `album_seq` (
+  `next_not_cached_value` bigint(21) NOT NULL,
+  `minimum_value` bigint(21) NOT NULL,
+  `maximum_value` bigint(21) NOT NULL,
+  `start_value` bigint(21) NOT NULL COMMENT 'start value when sequences is created or value if RESTART is used',
+  `increment` bigint(21) NOT NULL COMMENT 'increment value',
+  `cache_size` bigint(21) unsigned NOT NULL,
+  `cycle_option` tinyint(1) unsigned NOT NULL COMMENT '0 if no cycles are allowed, 1 if the sequence should begin a new cycle when maximum_value is passed',
+  `cycle_count` bigint(21) NOT NULL COMMENT 'How many cycles have been done'
+) ENGINE=InnoDB SEQUENCE=1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table albums.song
+CREATE TABLE IF NOT EXISTS `song` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `song` varchar(255) DEFAULT NULL,
+  `album_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKrcjmk41yqj3pl3iyii40niab0` (`album_id`),
+  CONSTRAINT `FKrcjmk41yqj3pl3iyii40niab0` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table albums.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Data exporting was unselected.
 
